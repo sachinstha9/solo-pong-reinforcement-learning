06d5e5f7c0af47f5432b540d71b47739fed48a0f
sachinstha9 <sachinshrestha7055@gmail.com>
2026-03-06 20:43:56 +1345

    Some updates

--- agent.py
+++ agent.py
@@ -1,13 +1,51 @@
 import flappy_bird_gymnasium
 import gymnasium
-env = gymnasium.make("FlappyBird-v0", render_mode="human", use_lidar=False)
+import torch
+from dqn import DQN
+from experience_replay import ReplayMemory
+import itertools
+import yaml
 
-obs, _ = env.reset()
+device = 'cuda' if torch.cuda.is_available() else 'cpu'
 
-while True:
-    action = env.action_space.sample()
-    obs, reward, terminated, _, info = env.step(action)
+class Agent:
+    def __init__(self, hyperparameter_set):
+        with open('hyperparameters.yml') as file:
+            all_hyperparamter_sets = yaml.safe_load(file)
+            hyperparameters = all_hyperparamter_sets[hyperparameter_set]
+        
+        self.mini_batch_size = hyperparameters["mini_batch_size"]
+        self.epsilon_init = hyperparameters["epsilon_init"]
+        self.epsilon_decay = hyperparameters["epsilon_decay"]
+        self.epsilon_min = hyperparameters["epsilon_min"]
 
-    if terminated:
-        break
-env.close()
+    def run(self, is_training=True, render=True):
+        env = gymnasium.make("FlappyBird-v0", render_mode="human" if render else None)
+
+        num_states = env.observation_space.shape[0]
+        num_actions = env.action_space.n
+
+        policy_dqn = DQN(num_states, num_actions).to_device(device)
+
+        if is_training:
+            memory = ReplayMemory(100_000)
+
+        reward_per_episode = []
+
+        for episode in itertools.count():
+            state, _ = env.reset()
+            terminated = False
+            episode_reward = 0.0
+
+            while not terminated:
+                action = env.action_space.sample()
+                new_state, reward, terminated, _, info = env.step(action)
+
+                episode_reward += reward
+
+                if is_training:
+                    memory.append((state, action, new_state, reward, terminated))
+
+                state = new_state
+
+            reward_per_episode.append(episode_reward)
