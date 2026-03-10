817e12edf23465b651abc8636d505f891d0b2d02
sachinstha9 <sachinshrestha7055@gmail.com>
2026-03-10 20:39:31 +1345

    game update function complete

--- game.py
+++ game.py
@@ -22,24 +22,65 @@ class Game:
         self.paddleColor = WHITE
         self.paddleSpeed = 10
 
-        self.ballRadius = 20
-        self.ballPosition = [self.ballRadius, self.ballRadius]
+        self.ballSize = 20
+        self.ballPosition = [self.ballSize, self.ballSize]
         self.ballColor = RED
         self.ballSpeed = [10, 8]
 
         self.reset()
 
     def reset(self):
-        self.ballPosition = [self.ballRadius, self.ballRadius]
+        self.ballPosition = [self.ballSize, self.ballSize]
         self.paddlePosition = [(WIDTH / 2)- (self.paddleWidth / 2), 900]
     
     def get_state(self):
         return [
-            self.paddlePosition[0],
+            self.paddlePosition[0] - (self.paddleWidth / 2),
             self.paddlePosition[1],
             self.ballPosition[0],
             self.ballPosition[1]
         ]
+
+    def step(self, action):
+        if action == 0:
+            self.paddlePosition[0] -= self.paddleSpeed # left
+        elif action == 2:
+            self.paddlePosition[0] += self.paddleSpeed # right
+
+        done = False
+
+        reward = 0
+
+        if (self.ballPosition[0] + (self.ballSize / 2) > self.paddlePosition[0] and self.ballPosition[0] - (self.ballSize / 2) < self.paddlePosition[0] + self.paddleWidth) and (self.ballPosition[1] + (self.ballSize / 2) >= self.paddlePosition[1]):
+            reward = 10
+        
+        if self.ballPosition[1] > HEIGHT:
+            reward = -10
+            done = True
+
+        return self.get_state(), reward, done
+
+    def update(self):
+        self.ballPosition[0] += self.ballSpeed[0]
+        self.ballPosition[1] += self.ballSpeed[1]
+
+        if (self.ballPosition[0] + (self.ballSize / 2) > self.paddlePosition[0] and self.ballPosition[0] - (self.ballSize / 2) < self.paddlePosition[0] + self.paddleWidth) and (self.ballPosition[1] + (self.ballSize / 2) > self.paddlePosition[1]):
+            self.ballPosition[1] = self.paddlePosition[1] - (self.ballSize / 2)
+            self.ballSpeed[1] *= -1
+
+        if (self.ballPosition[0] - (self.ballSize / 2) < 0):
+            self.ballPosition[0] = 0 + (self.ballSize / 2)
+            self.ballSpeed[0] *= -1
+        elif (self.ballPosition[0] + (self.ballSize / 2) > WIDTH):
+            self.ballPosition[0] = WIDTH - (self.ballSize / 2)
+            self.ballSpeed[0] *= -1
+        elif self.ballPosition[1] - (self.ballSize / 2) < 0:
+            self.ballPosition[1] = 0 + (self.ballSize / 2)
+            self.ballSpeed[1] *= -1
+
+        
+        print(self.ballSpeed[1])
+        self.draw()
 
     def draw(self):
         self.screen.fill(BLACK)
@@ -48,7 +89,7 @@ class Game:
         pygame.draw.rect(self.screen, self.paddleColor, (self.paddlePosition[0], self.paddlePosition[1], self.paddleWidth, self.paddleHeight))
 
         # ball
-        pygame.draw.circle(self.screen, self.ballColor, (self.ballPosition[0], self.ballPosition[1]), self.ballRadius)
+        pygame.draw.circle(self.screen, self.ballColor, (self.ballPosition[0], self.ballPosition[1]), self.ballSize)
 
         pygame.display.update()
         self.clock.tick(FPS)
@@ -60,4 +101,4 @@ while True:
         if e.type == pygame.QUIT:
             exit()
 
-    g.draw()
+    g.update()
